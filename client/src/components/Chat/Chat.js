66867811483e6b8878c186d2051fbd18c496d52e
Roy Ge <rgecoder@gmail.com>
2020-06-29 14:30:47 -0700

    added useEffect hook

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,11 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = ()=>{
-  return(
-    <h1>Chat</h1>
-  )
-}
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {
+
+    });
+
+    return ()=>{  //disconnect/unmount useEffect
+      socket.emit('disconnect');
+
+      socket.off();
+    }
+  }, [ENDPOINT, location.search]);
+
+  return <h1>Chat</h1>;
+};
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -13,8 +13,19 @@ const io = socketio(server);
 io.on('connection', (socket)=>{
   console.log('We have a new connection');
 
+  socket.on('join', ({name,room}, callback)=>{
+    console.log(name,room);
+  });
+
   socket.on('disconnect', ()=>{
     console.log('User has left');
+
+   
+    
+  })
+
+  socket.on('disconnect',()=>{
+    console.log('User had left');
   })
 })
 
